b675a05520a4d88c8ee895d3c99c85e72f29fc9b
wangzhen <wangzhen@curefun.com>
2019-09-24 18:03:27 +0800

    自定义view练习

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,11 +2,15 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
+
+    private Paint paint = new Paint();
 
     public Practice2DrawCircleView(Context context) {
         super(context);
@@ -14,10 +18,15 @@ public class Practice2DrawCircleView extends View {
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    private void init() {
+
     }
 
     @Override
@@ -26,5 +35,10 @@ public class Practice2DrawCircleView extends View {
 
 //        练习内容：使用 canvas.drawCircle() 方法画圆
 //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+
+        paint.setColor(Color.BLACK);
+        canvas.drawCircle(300, 300, 300, paint);
+
+
     }
 }
